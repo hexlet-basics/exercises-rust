c806fdd4a14d94fd7b4541e65223e78a89ba8eea
usernaimandrey <anshlyapnikov@yandex.ru>
2022-12-22 05:22:51 +0300

    add linter command and fix code

--- modules/10-basics/10-hello-world/test.rs
+++ modules/10-basics/10-hello-world/test.rs
@@ -1,9 +1,15 @@
 mod index;
+use std::io;
+
+#[allow(dead_code)]
+pub fn main() {
+    use index::greet;
+    greet(&mut io::stdout());
+}
 
 #[cfg(test)]
 mod tests {
     use index::greet;
-
     #[test]
     fn test_greet() {
         let mut stdout = Vec::new();
